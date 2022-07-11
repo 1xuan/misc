d7ff528cbd193fdb3503833a153586462c79ce99
yixuan <yixuan.coder@gmail.com>
2022-07-11 21:43:38 +0800

    add ubuntu_env_setup.sh

---
+++ ubuntu_env_setup.sh
@@ -0,0 +1,67 @@
+#
+# Usage:
+#   $ bash ubuntu_env_setup.sh -h
+#
+
+
+usage () {
+    echo 'Usage: bash ubuntu_env_setup.sh <command>'
+    echo 
+    echo 'commands:'
+    echo '  all     set up all env'
+}
+
+
+command=$1
+
+
+#
+# COLORs
+#
+# more info: https://www.shellhacks.com/Bash-Colors/
+#
+
+
+RED='\e[31m'
+END='\e[0m'
+
+red () {
+    echo '\e[31m'$1'\e[0m'
+}
+
+green () {
+    echo '\e[32m'$1'\e[0m'
+}
+
+# echo -e "[$(green 'OK')]"'pip install ok'
+
+
+function pip_setup {
+    # put the conf file in ~/.config/pip/
+    echo 'pip config...'
+    pip config set global.index-url https://pypi.douban.com/simple
+    if [ $? == 0 ]
+    then
+        echo -e "[$(green 'OK')]" "pip config"
+    else
+        echo -e "[$(red 'FAILED')]" "pip config"
+    fi
+}
+
+
+main () {
+    if [ -z $command ] || [ $command == '-h' ] || [ $command == '--help' ]; then
+        usage 
+        exit 1
+    elif [ $command == 'all' ]; then
+        pip_setup
+    else
+        echo -e "[$(red 'FAILED')] There is no such command ${command}"
+        echo
+        usage
+        exit 1
+    fi
+}
+
+main
+
